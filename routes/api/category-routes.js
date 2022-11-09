c099ccf0245e85d6f0d5f8a80124ba3e1de13f49
FDeaquino <fidel.deaquino@gmail.com>
2022-11-09 15:51:05 -0600

    Created Category routes: GET all Categories, GET one Category, DELETE a category; will come back to the PUT route that updates a category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,20 +1,64 @@
 const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const { Category, Product, Tag } = require('../../models');
 
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include: [
+      {
+        model: Product,
+        as: 'products'
+      }
+    ]
+  })
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    })
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Product,
+        as: 'products'
+      }
+    ]
+  })
+  .then(data => {
+    if (!data) {
+      // status code 404: client error, resource was not found
+      res.status(404).json({ message: 'Category with this id was not found.'});
+      return;
+    }
+    res.json(data);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create(req.body)
+  .then(data => res.json(data))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 router.put('/:id', (req, res) => {
@@ -23,6 +67,23 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(data => {
+    if (!data) {
+      // status code 404: client error, resource was not found
+      res.status(404).json({ message: 'Cannot delete. Category with this id was not found.'});
+      return;
+    }
+    res.json(data);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 module.exports = router;
