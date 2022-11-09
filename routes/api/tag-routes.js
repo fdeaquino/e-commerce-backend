7d1634a73d8e177149f259b7eb761e84ae8ef5ba
FDeaquino <fidel.deaquino@gmail.com>
2022-11-09 15:07:30 -0600

    Created Tag routes: GET all Tags, GET one Tag, DELETE one tag; will revisit PUT route to update tag at a later time

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,23 +6,85 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    include: [
+      {
+        model: Product,
+        as: 'products'
+      }
+    ]
+  })
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    })
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Product,
+        as: 'products'
+      }
+    ]
+  })
+    .then(data => {
+      if (!data) {
+        // status code 404: client error, resource was not found
+        res.status(404).json({ message: 'Tag with this id was not found.' });
+        return;
+      }
+      res.json(data);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    })
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create(req.body)
+    .then(data => res.json(data))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    })
 });
 
+// TODO: Request tutor help with this route - come back to it after looking at documentation - look at module project as reference
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(data => {
+    if (!data) {
+      // status code 404: client error, resource was not found
+      res.status(404).json({ message: 'Cannot delete. Tag with this id was not found.'});
+      return;
+    }
+    res.json(data);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 module.exports = router;
